d85b50ed00aa1e26ed953408ac3dd37691deccd9
MalikX <ibraheemmalliks@gmail.com>
2026-05-06 21:38:25 +0500

    Create upload_engine.py

---
+++ youtube-automation/upload_engine.py
@@ -0,0 +1,7 @@
+def upload(title, thumbnail, mode):
+    print("\n📤 UPLOAD SYSTEM")
+    print("TITLE:", title)
+    print("MODE:", mode)
+    print("THUMBNAIL:", thumbnail)
+
+    return "youtube_video_id_123"
