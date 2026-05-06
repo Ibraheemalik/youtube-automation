5cb99b3f8e251cd3c23281144a57b2b8a42c646d
MalikX <ibraheemmalliks@gmail.com>
2026-05-06 21:37:37 +0500

    Create audio_engine.py

---
+++ youtube-automation/audio_engine.py
@@ -0,0 +1,10 @@
+def get_audio():
+    return {
+        "music": "dark_ambient.mp3",
+        "sfx": [
+            "heartbeat.wav",
+            "whoosh.wav",
+            "door_creak.wav",
+            "low_rumble.wav"
+        ]
+    }
