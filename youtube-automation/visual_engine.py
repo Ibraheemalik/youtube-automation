87ab12f09f44607b6d512ade03243d189fada3c0
MalikX <ibraheemmalliks@gmail.com>
2026-05-06 21:37:15 +0500

    Create visual_engine.py

---
+++ youtube-automation/visual_engine.py
@@ -0,0 +1,9 @@
+import random
+
+def get_visuals():
+    return [
+        "pixabay_dark_room.mp4",
+        "pexels_fog_forest.mp4",
+        "wikimedia_empty_hall.jpg",
+        "silhouette_shadow.mp4"
+    ]
