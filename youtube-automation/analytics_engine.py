fcaeaacb8c7f246165f3f23745ce45bdaa45c418
MalikX <ibraheemmalliks@gmail.com>
2026-06-28 10:47:15 +0500

    Delete youtube-automation directory

--- youtube-automation/analytics_engine.py
+++
@@ -1,8 +0,0 @@
-import random
-
-def analytics():
-    return {
-        "ctr": round(random.uniform(4, 12), 2),
-        "avg_view": random.randint(30, 80),
-        "viral_score": random.randint(60, 95)
-    }
